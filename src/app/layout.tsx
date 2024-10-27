6fda6a01344b5967f12c9ee9303a122a5491dcdd
José Brito <josealbritose@gmail.com>
2024-10-27 09:06:53 -0300

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,28 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
-import './globals.css'
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: 'Camp Travel',
-  description: 'Travel UI/UX App for Camping',
-}
+  title: "Camp Travel",
+  description: "Travel UI/UX App for Camping",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <main className="relative overflow-hidden">
+          <Navbar />
           {children}
+          <Footer />
         </main>
       </body>
     </html>
-  )
+  );
 }
